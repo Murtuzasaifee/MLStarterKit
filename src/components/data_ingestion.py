d083c37cb44f061f88852d6df8bdd326237b3399
Murtuza Saifee <>
2025-01-19 14:55:18 +0400

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTranformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -43,10 +44,17 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
 
+
+## Test Code
+
 if __name__=="__main__":
     obj=DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTranformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_array, test_array,_ = data_transformation.initiate_data_transformation(train_data, test_data)
 
+    model_trainer = ModelTrainer()
+    score = model_trainer.initiate_model_trainer(train_array, test_array,_)
+    print(f'Final score is : {score}')
+    
